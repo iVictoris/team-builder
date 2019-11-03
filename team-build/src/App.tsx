211589f0c3c52956a0e98947c517b363f89a51ab
Victor Tran <victor.n.tran@outlook.com>
2019-11-03 15:43:28 -0800

    create state of team and render team of 1

--- team-build/src/App.tsx
+++ team-build/src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+  const [team, setTeam] = useState([
+    {
+      id: 1,
+      name: "Victor",
+      title: "web dev"
+    }
+  ]);
+
+  const teamMembers =
+    team.length > 0
+      ? team.map(member => {
+          return (
+            <>
+              <div className="name">{member.name}</div>
+              <div className="name">{member.title}</div>
+            </>
+          );
+        })
+      : null;
+
+  return <div className="App">{teamMembers}</div>;
+};
 
 export default App;
