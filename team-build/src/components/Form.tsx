873a8df6f7c455e2112c30d1840e6c269b07b1c1
Victor Tran <victor.n.tran@outlook.com>
2019-11-03 16:40:15 -0800

    move userInfo onto parent, create elements to submit form

--- team-build/src/components/Form.tsx
+++ team-build/src/components/Form.tsx
@@ -1,10 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, ChangeEvent, ReactNode } from "react";
+import { string } from "prop-types";
 
-const Form: React.FC = () => {
-  const [userInfo, setUserInfo] = useState({ name: "", title: "" });
-  return (<form onSubmit={e => e.preventDefault()}>
+interface iProps {
+  handleChange(event: React.ChangeEvent<HTMLInputElement>): void;
+  handleSubmit(event: React.FormEvent<HTMLFormElement>): void;
+  children?: ReactNode;
+  userInfo: {
+    [key: string]: string;
+    name: string;
+    email: string;
+    title: string;
+  };
+}
 
-  </form>);
+const Form: React.FC<iProps> = ({ handleChange, handleSubmit, userInfo }) => {
+  const { name, email, title } = userInfo;
+  return (
+    <form onSubmit={e => handleSubmit(e)} className="form">
+      <input
+        type="text"
+        name="name"
+        placeholder="Name"
+        value={name}
+        onChange={e => handleChange(e)}
+      />
+
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={email}
+        onChange={e => handleChange(e)}
+      />
+
+      <input
+        type="text"
+        name="title"
+        placeholder="Title"
+        value={title}
+        onChange={e => handleChange(e)}
+      />
+
+      <input type="submit" value="add member" />
+    </form>
+  );
 };
 
 export default Form;
