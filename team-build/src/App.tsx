f2508e2569f39a3564d3ce6da419c2b4f0126fb8
Victor Tran <victor.n.tran@outlook.com>
2019-11-03 16:41:08 -0800

    create interface to make documentation easier, create initial user state to make use of resetting state, make handlesubmit and handlechange and pass into form

--- team-build/src/App.tsx
+++ team-build/src/App.tsx
@@ -1,28 +1,58 @@
 import React, { useState } from "react";
 import "./App.css";
+import Form from "./components/Form";
+
+interface teamMember {
+  id: number,
+  name: string,
+  email: string,
+  title: string
+}
 
 const App: React.FC = () => {
-  const [team, setTeam] = useState([
-    {
-      id: 1,
-      name: "Victor",
-      title: "web dev"
-    }
-  ]);
+  let id: number = 0;
+  const initialUserInfo = { name: "", email: "", title: "" }
+  const [team, setTeam] = useState<teamMember[]>([]);
+
+  const [userInfo, setUserInfo] = useState({...initialUserInfo});
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setUserInfo({ ...userInfo, [name]: value });
+  };
 
   const teamMembers =
     team.length > 0
-      ? team.map(member => {
+      ? team.map((member: teamMember) => {
           return (
-            <>
+            <div key={member.id}>
               <div className="name">{member.name}</div>
+              <div className="name">{member.email}</div>
               <div className="name">{member.title}</div>
-            </>
+            </div>
           );
         })
       : null;
 
-  return <div className="App">{teamMembers}</div>;
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    setTeam([...team, { id: ++id, ...userInfo }]);
+    setUserInfo({...initialUserInfo})
+  };
+
+  return (
+    <div className="App">
+      <h2>Add Team Member Station:</h2>
+      <Form
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        userInfo={userInfo}
+      />
+      {teamMembers}
+    </div>
+  );
 };
 
 export default App;
